571f10ddc7c56ceacced59e31a8db892ee4959d6
oulievancs <cs151051@gmail.com>
2024-07-10 01:02:42 +0300

    Fix invalid iterable.

--- api/apiServer.py
+++ api/apiServer.py
@@ -196,7 +196,7 @@ def get_friends_of_user(user_id: int, connection: MySQLResult) -> set[int]:
     if res_friends.rowcount > 0:
         return {friend_id[0] for friend_id in res_friends.fetchall}
     else:
-        return set(-1)
+        return {-1}
 
 
 def get_artists_that_users_friends_listen(friend_user_ids: [int], connection: MySQLResult) -> set[int]:
@@ -234,7 +234,7 @@ def get_artists_in_users(user_ids: [int], filters: {"descriptors": set[int]}, co
     if res_artists.rowcount > 0:
         return {artist_id[0] for artist_id in res_artists.fetchall}
     else:
-        return set(-1)
+        return {-1}
 
 
 """Retrieve the artist's information with id's provied."""
@@ -297,7 +297,7 @@ def get_descriptors_regarding_user(user_id: int, connection: MySQLResult) -> set
     if res_listen.rowcount > 0:
         return {tpl[0] for tpl in res_listen.fetchall}
     else:
-        return set(-1)
+        return {-1}
 
 
 """Suggestions of artists regarding the artists that the user's email not has in
